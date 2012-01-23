1d2dd732cb006a64ccdb7e352cf9f05ef6344b76
fernandofederico1984 <fernandofederico1984@gmail.com>
2012-01-23 20:10:31 -0300

    First pages commit

---
+++ assets/navtree_data.js
@@ -0,0 +1,11 @@
+var NAVTREE_DATA =
+[ [ "org.mule.module.dynamicFlows", "org/mule/module/dynamicFlows/package-summary.html", [ [ "Classes", null, [ [ "DynamicFlowsModule", "org/mule/module/dynamicFlows/DynamicFlowsModule.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.dynamicFlows.config", "org/mule/module/dynamicFlows/config/package-summary.html", [ [ "Classes", null, [ [ "AddMessageProcessor", "org/mule/module/dynamicFlows/config/AddMessageProcessor.html", null, "" ], [ "DynamicFlowsModuleCapabilitiesAdapter", "org/mule/module/dynamicFlows/config/DynamicFlowsModuleCapabilitiesAdapter.html", null, "" ], [ "DynamicFlowsModuleLifecycleAdapter", "org/mule/module/dynamicFlows/config/DynamicFlowsModuleLifecycleAdapter.html", null, "" ], [ "RemoveMessageProcessor", "org/mule/module/dynamicFlows/config/RemoveMessageProcessor.html", null, "" ], [ "RunMessageProcessor", "org/mule/module/dynamicFlows/config/RunMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.dynamicFlows.config.spring", "org/mule/module/dynamicFlows/config/spring/package-summary.html", [ [ "Classes", null, [ [ "AddDefinitionParser", "org/mule/module/dynamicFlows/config/spring/AddDefinitionParser.html", null, "" ], [ "DynamicFlowsModuleConfigDefinitionParser", "org/mule/module/dynamicFlows/config/spring/DynamicFlowsModuleConfigDefinitionParser.html", null, "" ], [ "DynamicFlowsModuleNamespaceHandler", "org/mule/module/dynamicFlows/config/spring/DynamicFlowsModuleNamespaceHandler.html", null, "" ], [ "RemoveDefinitionParser", "org/mule/module/dynamicFlows/config/spring/RemoveDefinitionParser.html", null, "" ], [ "RunDefinitionParser", "org/mule/module/dynamicFlows/config/spring/RunDefinitionParser.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
